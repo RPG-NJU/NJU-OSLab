3d8a7fde63f493e4282be707a317275218df60a3
RPG <41836383+HELLORPG@users.noreply.github.com>
2019-05-24 00:41:39 +0800

    [update] 1.3 finished

--- lab4/app/main.c
+++ lab4/app/main.c
@@ -1,11 +1,14 @@
 #include "lib.h"
 #include "types.h"
 
+#define MODE 2
+
 int data = 0;
 
 int uEntry(void) 
 {
-    
+
+#if MODE == 1
     int i = 4;
     int ret = 0;
     sem_t sem;
@@ -51,6 +54,74 @@ int uEntry(void)
         sem_destroy(&sem);
         exit();
     }
+#endif
+
+#if MODE == 2
+    printf("[Start Test Producer & Consumer Program]\n");
+
+    sem_t buffer, lock;
+    int fork_time = 6;
+    int ret = 0;
+    int k = 1;
+    int j = 0;
+    int i = 0;
     
+    sem_init(&buffer, 0); sem_init(&lock, 1);
+
+    for (int i = 0; i < fork_time; ++i)
+    {
+        ret = fork();
+        if (ret == -1)
+            exit();
+    }
+    
+    if (getpid() == 2 || getpid() == 3)
+    {
+        j = getpid() - 1; // 编号从1开始进行编号
+        i = getpid();
+
+        for ( ; k <= 8; )
+        {
+            sleep(64);
+            printf("pid %d, producer %d, try lock, product %d\n", i, j, k);
+            sem_wait(&lock);
+            printf("pid %d, producer %d, locked\n", i, j);
+            //sleep(16);
+            sem_post(&buffer);
+            sleep(12);
+            printf("pid %d, prodecer %d, produce %d\n", i, j, k);
+            sem_post(&lock);
+            printf("pid %d, producer %d, unlock\n", i, j);
+            ++k;
+        }
+        exit();
+    }
+
+    else if (getpid() > 3 && getpid() < 8)
+    {
+        j = getpid() - 3; // 编号从1开始进行编号
+        i = getpid();
+
+        for ( ; k <= 4; )
+        {
+            printf("pid %d, consumer %d, try consume, product %d\n", i, j, k);
+            sem_wait(&buffer);
+            printf("pid %d, consumer %d, try lock, product %d\n", i, j, k);
+            sem_wait(&lock);
+            printf("pid %d, consumer %d, locked\n", i, j);
+            sleep(16);
+            printf("pid %d, consumer %d, consumed, product %d\n", i, j, k);
+            sem_post(&lock);
+            printf("pid %d, consumer %d, unlock\n", i, j);
+            sleep(160);
+            ++k;
+        }
+        exit();
+    }
+
+    // if (getpid() == 0)
+    //     printf("[End Test Producer & Consumer Program]\n");
+    exit();
+#endif
     return 0;
 }
